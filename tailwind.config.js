8eab2903ac90335cd8f62064f3ce3ea3572696c0
Nyamadi Atsu <devbrainy98@gmail.com>
2024-06-17 03:35:42 +0100

    init: setup fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -59,6 +59,9 @@ module.exports = {
         md: "calc(var(--radius) - 2px)",
         sm: "calc(var(--radius) - 4px)",
       },
+      fontFamily: {
+        roboto: ["Roboto Slab", "sans-serif", "serif"],
+      },
       keyframes: {
         "accordion-down": {
           from: { height: "0" },
