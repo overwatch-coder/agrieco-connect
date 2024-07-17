f242daf0f167cf1a1e76f5cbc94ddd8c2799f168
Nyamadi Atsu <devbrainy98@gmail.com>
2024-07-17 11:08:40 +0100

    chore: fix ref on custom login modal

--- src/components/shared/LoginModal.tsx
+++ src/components/shared/LoginModal.tsx
@@ -10,8 +10,8 @@ import {
   DialogClose,
 } from "@/components/ui/dialog";
 import { cn } from "@/lib/utils";
-
 import { useLocation, useNavigate } from "react-router-dom";
+import React, { forwardRef } from "react";
 
 type LoginWithChildrenProps = {
   children?: React.ReactNode;
@@ -28,67 +28,68 @@ type LoginWithButtonNameProps = {
 
 type PopUpLoginAlertProps = LoginWithChildrenProps & LoginWithButtonNameProps;
 
-const LoginModal = ({
-  buttonName,
-  className,
-  children,
-  hasChildren,
-  endPath,
-}: PopUpLoginAlertProps) => {
-  const navigate = useNavigate();
-  const pathname = useLocation().pathname;
+const LoginModal = forwardRef<HTMLButtonElement, PopUpLoginAlertProps>(
+  ({ buttonName, className, children, hasChildren, endPath }, ref) => {
+    const navigate = useNavigate();
+    const pathname = useLocation().pathname;
 
-  const handleRedirect = () => {
-    navigate(`/login?redirect=${pathname}${endPath ? endPath : ""}`);
-  };
+    const handleRedirect = () => {
+      navigate(`/login?redirect=${pathname}${endPath ? endPath : ""}`);
+    };
 
-  return (
-    <Dialog>
-      {hasChildren ? (
-        <DialogTrigger className={cn("", className)}>{children}</DialogTrigger>
-      ) : (
-        <DialogTrigger
-          className={cn(
-            "bg-primary-brown text-white px-7 py-3 rounded text-center",
-            className
-          )}
-        >
-          {buttonName}
-        </DialogTrigger>
-      )}
-
-      <DialogContent
-        id="hide"
-        className="flex flex-col gap-4 px-5 rounded-none"
-      >
-        <DialogHeader className="flex flex-col gap-3">
-          <DialogTitle className="flex items-center justify-center">
-            <span className="text-primary-brown text-2xl font-bold">
-              You must be logged in to do this
-            </span>
-          </DialogTitle>
-
-          <DialogDescription className="flex flex-col items-center gap-5">
-            <p className="text-secondary-gray font-semibold">
-              Do you want to login?
-            </p>
-          </DialogDescription>
-        </DialogHeader>
-
-        <DialogFooter className="md:flex-row flex flex-col items-center gap-5">
-          <DialogClose className="border-secondary-gray text-secondary-gray w-full py-2 border rounded">
-            Cancel
-          </DialogClose>
-          <Button
-            onClick={handleRedirect}
-            className="bg-primary-green w-full px-4 py-4 text-white"
+    return (
+      <Dialog>
+        {hasChildren ? (
+          <DialogTrigger ref={ref} className={cn("", className)}>
+            {children}
+          </DialogTrigger>
+        ) : (
+          <DialogTrigger
+            ref={ref}
+            className={cn(
+              "bg-primary-brown text-white px-7 py-3 rounded text-center",
+              className
+            )}
           >
-            Go to Login
-          </Button>
-        </DialogFooter>
-      </DialogContent>
-    </Dialog>
-  );
-};
+            {buttonName}
+          </DialogTrigger>
+        )}
+
+        <DialogContent
+          id="hide"
+          className="flex flex-col gap-4 px-5 rounded-none"
+        >
+          <DialogHeader className="flex flex-col gap-3">
+            <DialogTitle className="flex items-center justify-center">
+              <span className="text-primary-brown text-2xl font-bold">
+                You must be logged in to do this
+              </span>
+            </DialogTitle>
+
+            <DialogDescription className="flex flex-col items-center gap-5">
+              <p className="text-secondary-gray font-semibold">
+                Do you want to login?
+              </p>
+            </DialogDescription>
+          </DialogHeader>
+
+          <DialogFooter className="md:flex-row flex flex-col items-center gap-5">
+            <DialogClose className="border-secondary-gray text-secondary-gray w-full py-2 border rounded">
+              Cancel
+            </DialogClose>
+            <Button
+              onClick={handleRedirect}
+              className="bg-primary-green w-full px-4 py-4 text-white"
+            >
+              Go to Login
+            </Button>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
+    );
+  }
+);
+
+LoginModal.displayName = "LoginModal";
 
 export default LoginModal;
